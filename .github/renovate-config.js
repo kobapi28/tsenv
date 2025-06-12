03ac436dc497f421eb804df3ea5d639b35f30d35
kobapi28 <sho1228.k@gmail.com>
2025-06-13 01:21:20 +0900

    feat: enable automerge for patch updates

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -18,6 +18,7 @@ module.exports = {
     {
       matchUpdateTypes: ["patch"],
       labels: ["dependencies", "patch"],
+      automerge: true,
     },
     {
       matchDepTypes: ["devDependencies"],
